0ef21ea08e66977ca1550aff6068216212902b8d
Amith Wijesinghe <amithmwijesinghe@gmail.com>
2024-11-05 18:28:28 +0530

    job description show more & less based on useState

--- src/components/JobItem.jsx
+++ src/components/JobItem.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaMapMarker } from "react-icons/fa";
 
 const JobItem = ({ job }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let jobDescription = job.description;
+
+  if (!showFullDescription) {
+    jobDescription = job.description.slice(0, 200) + "...";
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative" key={job.id}>
       <div className="p-4">
@@ -9,13 +18,13 @@ const JobItem = ({ job }) => {
           <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
         <div className="mb-5">
-          <p>{job.description}</p>
+          <p>{jobDescription}</p>
         </div>
         <button
-          onClick={() => setShowFullDescription((prevState) => !prevState)}
           className="text-indigo-500 mb-5 hover:text-indigo-600"
+          onClick={() => setShowFullDescription((prevState) => !prevState)}
         >
-          {/* {showFullDescription ? "Less" : "More"} */}
+          {showFullDescription ? "Less" : "More"}
         </button>
 
         <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
@@ -24,7 +33,7 @@ const JobItem = ({ job }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            {/* <FaMapMarker className="inline text-lg mb-1 mr-1" /> */}
+            <FaMapMarker className="inline text-lg mb-1 mr-1"></FaMapMarker>
             {job.location}
           </div>
           <a
