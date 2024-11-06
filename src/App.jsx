030f61541961d322183682458cf2dc8a9a8b3a8b
Amith Wijesinghe <amithmwijesinghe@gmail.com>
2024-11-06 06:34:00 +0530

    Route and Link usage with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListing from "./components/JobListing";
-import ViewAllJobs from "./components/ViewAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
+// import HomeCards from "./components/HomeCards";
+// import JobListing from "./components/JobListing";
+// import ViewAllJobs from "./components/ViewAllJobs";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero
-        title="React Jobs Pool"
-        subtitle="Jobs that best fits your skills set!"
-      />
-      <HomeCards />
-      <JobListing />
-      <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />;
+  // return (
+  //   <>
+  //     <Navbar />
+  //     <Hero
+  //       title="React Jobs Pool"
+  //       subtitle="Jobs that best fits your skills set!"
+  //     />
+  //     <HomeCards />
+  //     <JobListing />
+  //     <ViewAllJobs />
+  //   </>
+  // );
 };
 
 export default App;
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const JobsPage = () => {
+  return <div>JOBS PAGE</div>;
+};
+
+export default JobsPage;
