29d859f6543128a3526f3810d07ad946a67899c1
Amith Wijesinghe <amithmwijesinghe@gmail.com>
2024-11-17 21:36:37 +0530

    add new job page and form submission

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,40 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Hero from "./components/Hero";
-// import HomeCards from "./components/HomeCards";
-// import JobListing from "./components/JobListing";
-// import ViewAllJobs from "./components/ViewAllJobs";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  )
-);
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 
 const App = () => {
+  // Add new job
+  const addJob = async (newJob) => {
+    const res = await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+
+    return;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router} />;
-  // return (
-  //   <>
-  //     <Navbar />
-  //     <Hero
-  //       title="React Jobs Pool"
-  //       subtitle="Jobs that best fits your skills set!"
-  //     />
-  //     <HomeCards />
-  //     <JobListing />
-  //     <ViewAllJobs />
-  //   </>
-  // );
 };
 
 export default App;
